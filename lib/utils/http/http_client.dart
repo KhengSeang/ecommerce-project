539a8f40188cc029b636abe12f6987b982658a79
KhengSeang <khengseangchoeng@gmail.com>
2025-01-04 12:24:39 +0700

    localStorage, loggin, validation

--- lib/utils/http/http_client.dart
+++ lib/utils/http/http_client.dart
@@ -10,6 +10,42 @@ class THttpHelper{
     return _handleResponse(response);
   }
 
+  // Helper method to make a POST request
+  static Future<Map<String, dynamic>> post(String endpoint, dynamic data) async {
+    final response = await http.post(
+    Uri.parse('$_baseUrl/$endpoint'),
+    headers: {'Content-Type': 'application/json'},
+    body: json.encode(data),
+    );
+    return _handleResponse(response);
+  }
+
+  // Helper method to make a PUT request
+  static Future<Map<String, dynamic>> put(String endpoint, dynamic data) async {
+    final response = await http.put(
+    Uri.parse('$_baseUrl/$endpoint'),
+    headers: {'Content-Type': 'application/json'},
+    body: json.encode(data),
+    );
+    return _handleResponse(response);
+  }
+
+  // Helper method to make a DELETE request
+  static Future<Map<String, dynamic>> delete(String endpoint) async {
+    final response = await http.delete(
+      Uri.parse('$_baseUrl/$endpoint'));
+      return _handleResponse(response);
+  }
+
+  // Handle the HTTP response
+  static Future<Map<String, dynamic>> handleResponse(http.Response response){
+    if(response.statusCode == 200){
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to fetch data: ${response.statusCode}');
+    }
+  }
+
   static Map<String, dynamic> _handleResponse(http.Response response){
     if (response.statusCode == 200) {
       return json.decode(response.body);
