8d323e08d77048db87ae46c086805eb8fbf62191
KhengSeang <khengseangchoeng@gmail.com>
2025-01-07 16:42:03 +0700

    set_up_firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
 import 'package:t_store/app.dart';
+import 'package:t_store/firebase_options.dart';
 
-void main() {
+
+/// -- Entry point of flutter app
+Future<void> main() async {
+  
+
+  // Todo: Add Widgets Bindings
+  // Todo: Init Local Storage
+  // Todo: Await Native Splash
+  // Todo: Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
+
+
+  // Todo: Initialize Authentication
+
+
+
   runApp(const App());
+
 }
