929865164942d668dad74ccff920a11b1b6c752b
Visothpoev <140947691+Visothpoev@users.noreply.github.com>
2025-01-13 18:27:22 +0700

    Form fix

--- lib/features/authentication/screens/login/widget/login_form.dart
+++ lib/features/authentication/screens/login/widget/login_form.dart
@@ -14,65 +14,67 @@ class TLoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: TSizes.spaceBtwSections),
-      child: Form(
-          child: Column(
-        children: [
-          //Email
-          TextFormField(
+    return Form(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: TSizes.spaceBtwSections),
+        child: Form(
+            child: Column(
+          children: [
+            //Email
+            TextFormField(
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Iconsax.direct_right),
+                    labelText: TTexts.email)),
+            const SizedBox(height: TSizes.spaceBtwInputFields),
+
+            //Password
+            TextFormField(
               decoration: const InputDecoration(
-                  prefixIcon: Icon(Iconsax.direct_right),
-                  labelText: TTexts.email)),
-          const SizedBox(height: TSizes.spaceBtwInputFields),
+                  prefixIcon: Icon(Iconsax.password_check),
+                  labelText: TTexts.password,
+                  suffixIcon: Icon(Iconsax.eye_slash)),
+            ),
+            const SizedBox(height: TSizes.spaceBtwInputFields),
 
-          //Password
-          TextFormField(
-            decoration: const InputDecoration(
-                prefixIcon: Icon(Iconsax.password_check),
-                labelText: TTexts.password,
-                suffixIcon: Icon(Iconsax.eye_slash)),
-          ),
-          const SizedBox(height: TSizes.spaceBtwInputFields),
+            //Remember Me & Forgot Password
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //Remember Me
+                Row(
+                  children: [
+                    Checkbox(value: true, onChanged: (value) {}),
+                    const Text(TTexts.rememberMe),
+                  ],
+                ),
 
-          //Remember Me & Forgot Password
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              //Remember Me
-              Row(
-                children: [
-                  Checkbox(value: true, onChanged: (value) {}),
-                  const Text(TTexts.rememberMe),
-                ],
-              ),
+                //Forgot Password
+                TextButton(
+                    onPressed: () => Get.to(() => const ForgetPassword()),
+                    child: const Text(TTexts.forgetPassword))
+              ],
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
 
-              //Forgot Password
-              TextButton(
-                  onPressed: () => Get.to(() => const ForgetPassword()),
-                  child: const Text(TTexts.forgetPassword))
-            ],
-          ),
-          const SizedBox(height: TSizes.spaceBtwSections),
+            //Sign In Button
+            SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => Get.to(() => const NavigationMenu()),
+                  child: const Text(TTexts.signIn),
+                )),
+            const SizedBox(height: TSizes.spaceBtwItems),
 
-          //Sign In Button
-          SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () => Get.to(() => const NavigationMenu()),
-                child: const Text(TTexts.signIn),
-              )),
-          const SizedBox(height: TSizes.spaceBtwItems),
-
-          //Create Account Button
-          SizedBox(
-              width: double.infinity,
-              child: OutlinedButton(
-                onPressed: () => Get.to(() => const SignupScreen()),
-                child: const Text(TTexts.createAccount),
-              ))
-        ],
-      )),
+            //Create Account Button
+            SizedBox(
+                width: double.infinity,
+                child: OutlinedButton(
+                  onPressed: () => Get.to(() => const SignupScreen()),
+                  child: const Text(TTexts.createAccount),
+                ))
+          ],
+        )),
+      ),
     );
   }
 }
